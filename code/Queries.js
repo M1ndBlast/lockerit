ed3e2a36bcb5c552873449c2c9ff7b3c609425d7
M1ndBlast <gustavopdzz0@gmail.com>
2022-12-06 01:44:49 -0600

    Validacion de registros e inicio de sesion

--- code/Queries.js
+++ code/Queries.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 var con = mysql.createConnection({
-	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PASSWORD,
-	database: process.env.MYSQL_DATABASE,
+	host:		process.env.MYSQL_HOST,
+	port:		process.env.MYSQL_PORT,
+	user: 		process.env.MYSQL_USER,
+	password:	process.env.MYSQL_PASSWORD,
+	database:	process.env.MYSQL_DATABASE,
 });
 
 // loop if fails to connect
@@ -13,80 +13,73 @@ function connect() {
 	con.connect((err) => {
 		if (err) {
 			console.error(err);
-			console.log('Error. Intentando de nuevo...');
+			console.log('Connection to MySQL failed. Retrying in 2 seconds...');
 			setTimeout(connect, 2*1000);	// try again in 2 seconds
-		}
-		else console.log(`Conectado a BD!`);
+		} 
+		else console.log(`Connected to MySQL on ${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}!`);
 	});
 }
 
 connect();
 
-con.on('error', function (err) {
-	console.log('Error', err);
-	if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNREFUSED') {
+con.on('error', function(err) {
+	console.log('db error', err);
+	if(err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET' || err.code === 'ECONNREFUSED') 
 		connect();
-	} else {
+	else
 		throw err;
-	}
 });
 
 // Here goes MySQL queries
 
-const bd = {
-	ROL_ADMIN: 1,
-	ROL_REPARTIDOR: 2,
-	ROL_CLIENTE: 3,
-
+const db = {
+	// Roles
+	ROLES: {
+		ADMIN: 1,
+		DELIVERY: 2,
+		CLIENT: 3,
+	},
 	/**
+	 * 
 	 * @param {string} email 
-	 * @param {string} contra 
+	 * @param {string} password 
 	 * @returns {Promise}
 	 */
-	iniciarSesion: (email, contra) => {
+	checkCredentials: (email, password) => {
 		return new Promise((resolve, reject) => {
-			con.query('SELECT * FROM User WHERE em_usr = ? AND pwd_usr = ? LIMIT 1', [email, contra], (err, results) => {
+			con.query('SELECT * FROM User WHERE correo = ? AND password = ? LIMIT 1', [email, password], (err, results) => {
 				if (err) reject(err);
 				else resolve(results);
 			});
 		});
 	},
-	// get all users
-	usuarios: () => {
+	getClientById: (id) => {
 		return new Promise((resolve, reject) => {
-			con.query('SELECT * FROM User', (err, results) => {
-				if (err) reject(err);
-				else resolve(results);
-			});
-		});
-	},
-	// get user by id
-	usuarioId: (id) => {
-		return new Promise((resolve, reject) => {
-			con.query('SELECT * FROM User WHERE id_usr = ? LIMIT 1', [id], (err, results) => {
-				if (err) reject(err);
-				else resolve(results);
-			});
-		});
-	},
-	usuarioEmail: (email) => {
-		return new Promise((resolve, reject) => {
-			con.query('SELECT * FROM User WHERE em_usr = ? LIMIT 1', [email], (err, results) => {
+			con.query('SELECT * FROM Cliente WHERE id_cliente = ? LIMIT 1', [id], (err, results) => {
 				if (err) reject(err);
 				else resolve(results);
 			});
 		});
 	},
 
+	getClientByEmail: (correo) => {
+		return new Promise((resolve, reject) => {
+			con.query('SELECT * FROM Cliente WHERE correo = ? LIMIT 1', [correo], (err, results) => {
+				if (err) reject(err);
+				else resolve(results);
+			});
+		});
+	},
+	
 	// create new user with validation
-	crearCuenta: (nom, email, tel, contra, type) => {
+	createClient: (nombre, apellidoP, apellidoA, celular, correo, password, id_tipoUsuario) => {
 		return new Promise((resolve, reject) => {
 			// check if email is already in use
-			bd.usuarioEmail(email).then((results) => {
+			db.getClientByEmail(correo).then((results) => {
 				if (results.length > 0) reject('Correo ya en uso');
 				else {
 					// create new user
-					con.query('INSERT INTO User VALUES (NULL, ?, ?, ?, ?, ?)', [nom, email, tel, contra, type], (err, results) => {
+					con.query('INSERT INTO Cliente VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)', [nombre, apellidoP, apellidoA, celular, correo, password, id_tipoUsuario], (err, results) => {
 						if (err) reject(err);
 						else resolve(results);
 					});
@@ -100,5 +93,4 @@ const bd = {
 
 
 // End of queries
-
-module.exports = bd;
+module.exports = db;
