165671fd7c42d3365f284fb796e03dd821bb5900
MarcoTL <marko274.matl@gmail.com>
2022-12-08 16:57:27 -0600

    Listo

--- code/public/js/card.js
+++ code/public/js/card.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
-    $('input:radio').change(function () {//Clicking input radio
-        var radioClicked = $(this).attr('id');
+    $('.card').change(function () {//Clicking input radio
+        var radioClicked = $(this).find('input[name=listGroupRadio]').attr('id');
         unclickRadio();
         removeActive();
         clickRadio(radioClicked);
         makeActive(radioClicked);
     });
     $(".card").click(function () {//Clicking the card
-        var inputElement = $(this).find('input[type=radio]').attr('id');
+        var inputElement = $(this).find('input[name=paquete]').attr('id');
+        unclickRadio();
+        removeActive();
+        makeActive(inputElement);
+        clickRadio(inputElement);
+    });
+    $(".card").click(function () {//Clicking the card
+        var inputElement = $(this).find('input[name=tipo]').attr('id');
         unclickRadio();
         removeActive();
         makeActive(inputElement);
@@ -16,8 +23,8 @@ $(document).ready(function () {
 });
 
 
-function unclickRadio() {
-    $("input:radio").prop("checked", false);
+function unclickRadio(inputElement) {
+    $("#" + inputElement).prop("checked", false);
 }
 
 function clickRadio(inputElement) {
